8440df80763c47b36c83b7de35342cc1610fe4e7
kenishiky <89154259+Radoharivola@users.noreply.github.com>
2021-12-17 09:23:16 +0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,16 +1,31 @@
-create table test(
-    nom varchar(255) not null
+create table typeSignalement (
+    id int serial primary key,
+    -- id int identity primary key, ho an Sitraka ihany
+    nom varchar(25) not null
+);
+create table affectation (
+    -- aleo refa anao anle affectation lo ty vao atao ka XD
 );
 
-insert into test(nom) values('kenny');
-insert into test(nom) values('sitraka');
-insert into test(nom) values('bobota');
-insert into test(nom) values('chantony');
-create table test(
-    nom varchar(255) not null
+create table signalement(
+    id int serial primary key,
+    -- id int identity primary key, ho an Sitraka ihany
+    idUser int not null,
+    idType int not null,
+    -- imageSignalement binary not null, ho an Sitraka
+    imageSignalement bytea not null,
+    longitude float not null,
+    latitude float not null,
+    descriptionSignalement text not null,
+    foreign key (idType) references typeSignalement(id)
 );
 
-insert into test(nom) values('kenny');
-insert into test(nom) values('sitraka');
-insert into test(nom) values('bobota');
-insert into test(nom) values('chantony');
+
+-- create table test(
+--     nom varchar(255) not null
+-- );
+
+-- insert into test(nom) values('kenny');
+-- insert into test(nom) values('sitraka');
+-- insert into test(nom) values('bobota');
+-- insert into test(nom) values('chantony');
