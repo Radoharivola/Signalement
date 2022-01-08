82cb415042cd56eb196d75ff5a2e41e9766c9c7d
kenishiky <89154259+Radoharivola@users.noreply.github.com>
2022-01-08 11:59:32 +0300

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -4,58 +4,50 @@ create table typeSignalement (
     nom varchar(25) not null
 );
 
+
+insert into typeSignalement values('1','Lalana');
+insert into typeSignalement values('2','Fanolanana');
+insert into typeSignalement values('3','Andrebabe');
+
 create table region(
     id serial primary key,
     nom varchar(255)
 );
 
 
+insert into region(nom) values('Analamanga');
+insert into region(nom) values('Ampafandrana');
+insert into region(nom) values('Apindray');
+insert into region(nom) values('Ampofy');
+insert into region(nom) values('Ambolomanga');
+insert into region(nom) values('Ambohibonga');
+insert into region(nom) values('ForangaBory');
+
 create table signalement(
     id serial primary key,
-    -- id int identity primary key, ho an Sitraka ihany
     idUser int not null,
     idType int not null,
     idRegion int,
-
-    --idRegion int not null,
-    -- imageSignalement binary not null, ho an Sitraka
     longitude float not null,
     latitude float not null,
     descriptionSignalement text not null,
     dateSignalement timestamp not null,
-    foreign key (idType) references typeSignalement(id),
-    foreign key(idRegion) references region(id);
-    --foreign key( idRegion) references region(id)
+    foreign key(idType) references typeSignalement(id),
+    foreign key(idRegion) references region(id)
 );
 
-create table 
-
-insert into typeSignalement values('1','Lalana');
-insert into typeSignalement values('2','Fanolanana');
-insert into typeSignalement values('3','Andrebabe');
 
 
-insert into signalement values('1','1','1','image1.jpg','24.3421','21.343223','this is a test','2000-12-12 00:00:00');
-insert into signalement values('2','1','2','image2.jpg','24.3421','21.343223','this is a test 2','2000-12-12 00:00:00');
+insert into signalement values('1','1','1','1','24.3421','21.343223','this is a test','2000-12-12 00:00:00');
+insert into signalement values('2','1','2','1','24.3421','21.343223','this is a test 2','2000-12-12 00:00:00');
 
-insert into signalement values('3','1','1','image3.jpg','24.3421','21.343223','this is a test 3','2000-12-12 00:00:00');
-insert into signalement values('4','1','3','image4.jpg','24.3421','21.343223','this is a test 4','2000-12-12 00:00:00');
-insert into signalement values('5','1','3','image5.jpg','24.3421','21.343223','this is a test 5','2000-12-12 00:00:00');
-
-
-insert into typeSignalement(nom) values('Infrastructure');
-insert into typeSignalement(nom) values('Mpanolana');
+insert into signalement values('3','1','1','1','24.3421','21.343223','this is a test 3','2000-12-12 00:00:00');
+insert into signalement values('4','1','3','1','24.3421','21.343223','this is a test 4','2000-12-12 00:00:00');
+insert into signalement values('5','1','3','1','24.3421','21.343223','this is a test 5','2000-12-12 00:00:00');
 
 
 
 
-insert into region(nomRegion) values('Analamanga');
-insert into region(nomRegion) values('Ampafandrana');
-insert into region(nomRegion) values('Apindray');
-insert into region(nomRegion) values('Ampofy');
-insert into region(nomRegion) values('Ambolomanga');
-insert into region(nomRegion) values('Ambohibonga');
-insert into region(nomRegion) values('ForangaBory');
 
 
 create table Test2(
@@ -71,10 +63,8 @@ insert into Test2 values('3','Jebe','343548964');
 
 create table imageSignalement (
     id serial primary key,
-    -- id int identity primary key, ho an Sitraka ihany
     idSignalement int not null,
     imageSignalement varchar(255) not null,
-    -- imageSignalement binary not null, ho an Sitraka ihany
     foreign key (idSignalement) references signalement(id)
 );
 
@@ -84,7 +74,6 @@ insert into imageSignalement(idSignalement,imageSignalement) values('2','image2.
 
 create table enCours(
     id serial primary key,
-    -- id int identity primary key, ho an katsa ihany
     idSignalement int not null,
     foreign key(idSignalement) references signalement(id)
 );
@@ -96,7 +85,6 @@ insert into enCours values('2','3');
 
 create table termine(
     id serial primary key,
-    -- id int identity primary key, ho an Sitraka ihany
     idSignalement int not null,
     foreign key(idSignalement) references signalement(id)
 );
