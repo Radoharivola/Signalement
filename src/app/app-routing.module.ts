6dc071b3dc6db257b78c9356b369283073ed6763
kenishiky <89154259+Radoharivola@users.noreply.github.com>
2022-01-17 00:35:58 +0300

    hehehehehehehehehehehe afaka matory amzay lelikeeeeee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListSignalementComponent } from './pages/body/list-signalement/list-signalement.component';
 import { AdminComponent } from './pages/body/admin/admin.component';
 import { AuthComponent } from './pages/auth/auth.component';
+import { MapComponent } from './pages/body/map/map.component';
 
 const routes: Routes = [
   { path: 'list', component: ListSignalementComponent },
   { path: 'admin', component: AdminComponent },
   { path: 'login', component: AuthComponent },
+  { path:'map',component:MapComponent}
 ];
 
 @NgModule({
--- src/app/pages/body/map/map.component.ts
+++ src/app/pages/body/map/map.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import Map from 'ol/Map';
+import Tile from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+import { fromLonLat } from 'ol/proj';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
+  map: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.initMap();
   }
-
+  initMap(): void {
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new Tile({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: fromLonLat([47.4424745,-18.887503]),
+        zoom: 6.4
+      })
+    });
+  }
 }
