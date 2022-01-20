f92861d93c6160ef9288c48d36da9cfe573ad9ec
RADOHARIVOLA Anjatiana Kenny Brian <89154259+Radoharivola@users.noreply.github.com>
2022-01-20 10:42:16 +0300

    Update TypeSignalement.java

--- src/main/java/signalement/app/Models/TypeSignalement.java
+++ src/main/java/signalement/app/Models/TypeSignalement.java
@@ -1,10 +1,13 @@
 package signalement.app.Models;
+import java.sql.*;
 
 public class TypeSignalement  extends Fonctions{
     Integer id;
     String nom;
+    Integer etat;
 
     public TypeSignalement() {
+        etat=1;
     }
 
     public String get_Nom() {
@@ -24,5 +27,40 @@ public class TypeSignalement  extends Fonctions{
         return id;
     }
 
+    public void set_Etat(Object etat){
+        this.etat=Integer.valueOf(String.valueOf(etat));
+
+    }
+
+    public Integer get_Etat(){
+        return etat;
+    }
+
+
+      public void update(Connection con){
+        try{
+
+            java.sql.Statement stmt = con.createStatement();
+            stmt.executeUpdate("Update typeSignalement set nom='"+this.get_Nom()+"' where id='"+this.get_Id()+"'");
+            stmt.executeUpdate("commit");
+            stmt.close();
+        }
+        catch(Exception e ){
+            e.printStackTrace();
+        }
+      }
+
+      public void deleteData(Connection con){
+        try{
+
+            java.sql.Statement stmt = con.createStatement();
+            stmt.executeUpdate("Update typeSignalement set etat=0 where id='"+this.get_Id()+"'");
+            stmt.executeUpdate("commit");
+            stmt.close();
+        }
+        catch(Exception e ){
+            e.printStackTrace();
+        }
+    }
 
 }
