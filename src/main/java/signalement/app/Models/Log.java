53a670efd7ef7f3a2bc3cc9738891bd131e7fdeb
kenishiky <89154259+Radoharivola@users.noreply.github.com>
2022-02-23 22:55:49 +0300

    Update Log.java

--- src/main/java/signalement/app/Models/Log.java
+++ src/main/java/signalement/app/Models/Log.java
@@ -5,9 +5,9 @@ public class Log{
     public Log() throws Exception{
         try{
             Class.forName("org.postgresql.Driver");
-            String url="jdbc:postgresql://localhost:5432/signalement";
-	   		String user="postgres";
-	   		String password="root";
+            String url="jdbc:postgresql://ec2-34-236-34-103.compute-1.amazonaws.com:5432/dedpq4ce0knhdp";
+	   		String user="yybwsfvuyxnjwo";
+	   		String password="277b668d95cb7d82531039d6d0e5c0ec1be3dec10b9878504c67b62fe6d425ed";
 	   		con=DriverManager.getConnection(url, user, password);
             con.setAutoCommit(false);
         }
