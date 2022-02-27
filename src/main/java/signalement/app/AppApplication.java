bd11eee8d6d98466c7f5216c025952235ac085f2
RADOHARIVOLA Anjatiana Kenny Brian <89154259+Radoharivola@users.noreply.github.com>
2022-02-27 10:16:05 +0300

    Update AppApplication.java

--- src/main/java/signalement/app/AppApplication.java
+++ src/main/java/signalement/app/AppApplication.java
@@ -2,8 +2,12 @@ package signalement.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import signalement.app.Models.UserTokenRepository;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = UserTokenRepository.class)
 public class AppApplication {
 
 	public static void main(String[] args) {
